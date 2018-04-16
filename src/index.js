78c11f857e8a28314e85eef4d22a4daa0102f61f
Jeffrey Park <jypark03@gmail.com>
2018-04-16 03:17:08 -0400

    added a sources page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-// var url = document.getElementById('url').innerHTML;	
-var url = "google.com";
+var url = document.getElementById('url').innerHTML;	
+// var url = "google.com";
 
 console.log(url);
 
